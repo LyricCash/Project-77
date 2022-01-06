8dc5c1dba63e94916d249151ca04c06f5394efd9
LyricCash <88355435+LyricCash@users.noreply.github.com>
2022-01-05 19:36:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var first_name = "Steve";
+var last_name = " Jobs";
+first_name.concat(last_name);
+
+var a = 14;
+var b = 7;
+
+var add = a + b;
+console.log(add);
+var subtract = a - b;
+console.log(subtract);
